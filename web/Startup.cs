9998d782775c0c5d2d6c376ec8435d64c89e4701
Zhang Yang Richie <yrzhang@thoughtworks.com>
2017-06-27 13:54:53 +0800

    new web

--- web/Startup.cs
+++ web/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace web1.app
+namespace web
 {
     public class Startup
     {
